0900dee4971665688879d68b92de7a26a4fcd075
abbossth <bosskodevelopment@gmail.com>
2023-09-08 19:56:12 +0500

    Configured App.jsx and created Protected Route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import "./assets/css/media.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/layout";
 import Home from "./pages/home";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
+  const loggedIn = false;
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />}></Route>
-      </Route>
-    </Routes>
+    <>
+      <ProtectedRoute isSignedIn={!loggedIn}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<h2>Test</h2>} />
+            <Route path="*" element={<h2>Not Found Auth</h2>}></Route>
+          </Routes>
+        </Layout>
+      </ProtectedRoute>
+      {loggedIn && (
+        <Routes>
+          <Route path="/login" element={<h2>Login</h2>}></Route>
+          <Route path="*" element={<h2>Not Found</h2>}></Route>
+        </Routes>
+      )}
+    </>
   );
 };
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,12 @@
+import Logo from "../assets/img/logo.svg";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <img src={Logo} alt="logo" />
+      Navbar
+    </div>
+  );
 };
 
 export default Navbar;
