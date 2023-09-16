fcc4dc9ee58c77b6024a1acff7e6358a1a4e2b33
abbossth <bosskodevelopment@gmail.com>
2023-09-16 18:27:57 +0500

    little bug fix

--- src/components/modals/signUp.js
+++ src/components/modals/signUp.js
@@ -26,6 +26,7 @@ const SignUp = () => {
               <div className="es-modal-content-header">
                 <svg
                   width="32"
+                  height="32"
                   viewBox="0 0 32 32"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
