f31d5ba922a5f0761537e6db16f34bf2363ea306
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-09-17 14:42:37 +0500

    Dropdown + Sign out

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -3,23 +3,10 @@ import { logOut } from "../store/actions/loginAction";
 import { loadMoreArticles } from "../store/actions/articleAction";
 
 const ProtectedRoute = ({ isSignedIn, children }) => {
-  const dispatch = useDispatch();
-  const logOutHandler = () => {
-    dispatch(logOut());
-  };
-  const loadMoreHandler = () => {
-    dispatch(loadMoreArticles());
-  };
   if (isSignedIn) {
     return (
       <>
         {children}
-        <button type="button" onClick={logOutHandler}>
-          Log out
-        </button>
-        <button type="button" onClick={loadMoreHandler}>
-          Load More Articles
-        </button>
       </>
     );
   }
