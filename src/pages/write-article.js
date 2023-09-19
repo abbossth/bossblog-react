62e5a51c6497a0f87d7a2b716626aef583eac2a6
abbossth <bosskodevelopment@gmail.com>
2023-09-20 04:48:15 +0500

    Write page and Editor JS Setup

--- src/pages/write-article.js
+++ src/pages/write-article.js
@@ -1,26 +1,34 @@
 import React from "react";
+import WritePanel from "../components/writePanel";
 
 const WriteArticle = () => {
   return (
-    <main>
-      <section className="es-article-header es-regular-section">
-        <div className="es-av-header">
-          <div className="container">
-            <div className="es-av-title">Maqola yozish</div>
-            <nav aria-label="breadcrumb">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item">
-                  <a href="#">Asosiy</a>
-                </li>
-                <li className="breadcrumb-item active" aria-current="page">
-                  Maqola yozish
-                </li>
-              </ol>
-            </nav>
+    <>
+      <main>
+        <section className="es-article-header es-regular-section">
+          <div className="es-av-header">
+            <div className="container">
+              <div className="es-av-title">Maqola yozish</div>
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item">
+                    <a href="#">Asosiy</a>
+                  </li>
+                  <li className="breadcrumb-item active" aria-current="page">
+                    Maqola yozish
+                  </li>
+                </ol>
+              </nav>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+        <section className="es-regular-section">
+          <div className="container">
+            <WritePanel />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
---
+++ src/plugins/editor.js
@@ -0,0 +1,101 @@
+import axios from "../api/axios";
+import Embed from "@editorjs/embed";
+import Table from "@editorjs/table";
+import List from "@editorjs/list";
+import Warning from "@editorjs/warning";
+import Code from "@editorjs/code";
+import LinkTool from "@editorjs/link";
+import Image from "@editorjs/image";
+import Raw from "@editorjs/raw";
+import Header from "@editorjs/header";
+import Quote from "@editorjs/quote";
+import Marker from "@editorjs/marker";
+import CheckList from "@editorjs/checklist";
+import Delimiter from "@editorjs/delimiter";
+import InlineCode from "@editorjs/inline-code";
+import SimpleImage from "@editorjs/simple-image";
+
+export const EDITOR_JS_TOOLS = {
+  // NOTE: Paragraph is default tool. Declare only when you want to change paragraph option.
+  // paragraph: Paragraph,
+  embed: {
+    class: Embed,
+    inlineToolbar: true,
+  },
+  table: {
+    class: Table,
+    inlineToolbar: true,
+  },
+  list: {
+    class: List,
+    inlineToolbar: true,
+  },
+  warning: {
+    class: Warning,
+    inlineToolbar: true,
+  },
+  code: {
+    class: Code,
+    inlineToolbar: true,
+  },
+  linkTool: {
+    class: LinkTool,
+    inlineToolbar: true,
+  },
+  image: {
+    class: Image,
+    config: {
+      uploader: {
+        async uploadByFile(file) {
+          const formData = new FormData();
+          formData.append("image", file);
+          const response = await axios.post(`/upload/image`, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            withCredentials: false,
+          });
+          return response.data;
+        },
+        async uploadByUrl(url) {
+          const response = await axios.post(`/upload/upload-by-url`, {
+            url,
+          });
+          return response.data;
+        },
+      },
+    },
+  },
+  raw: {
+    class: Raw,
+    inlineToolbar: true,
+  },
+  header: {
+    class: Header,
+    inlineToolbar: true,
+  },
+  quote: {
+    class: Quote,
+    inlineToolbar: true,
+  },
+  marker: {
+    class: Marker,
+    inlineToolbar: true,
+  },
+  checklist: {
+    class: CheckList,
+    inlineToolbar: true,
+  },
+  delimiter: {
+    class: Delimiter,
+    inlineToolbar: true,
+  },
+  inlineCode: {
+    class: InlineCode,
+    inlineToolbar: true,
+  },
+  simpleImage: {
+    class: SimpleImage,
+    inlineToolbar: true,
+  },
+};
